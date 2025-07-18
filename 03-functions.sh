cd398815ec8802f861625b60f19bcf0b91aca0a2
Anil Katari <anilkataricloudengineer@gmail.com>
2025-07-18 22:16:45 +0530

    Roboshop Project

---
+++ 03-functions.sh
@@ -0,0 +1,14 @@
+# If you give a name to set of commands then it is called as function.
+
+# Syntax: Declare a function
+# function_name() {
+#   commands
+# }
+
+# Call a function
+# sample
+
+sample() {
+  ls -l
+  pwd
+}
