5a4b10be144c6f40226fb2c50cbb70f8fd0e2a10
Anil Katari <anilkataricloudengineer@gmail.com>
2025-07-25 16:26:43 +0530

    Roboshop Project

---
+++ 07-inputs.sh
@@ -0,0 +1,19 @@
+# If a script needs a input then it can be done in these three ways
+
+# LHS to Script
+# Syntax: a=10 b=20 bash script
+# Access the variable as $a $b
+
+# RHS to Script
+# Syntax: bash script 10 20
+# Access the variables as $1 $2 and so on
+
+# Inherit from Parent (Which is Shell)
+# export a=10
+# bash script
+
+# Example for LHS
+
+echo LHS Values a - $a , b -$b
+
+
